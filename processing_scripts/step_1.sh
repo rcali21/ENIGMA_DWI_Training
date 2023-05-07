5167d49d9c6f30065e3fe4627ae63d027fb4e886
Ryan Cali <ryancali@PaulThoRyansMBP.fios-router.home>
2023-05-06 18:18:17 -0700

    Created step_1 dwidenoise script

--- processing_scripts/step_1.sh
+++ processing_scripts/step_1.sh
@@ -7,7 +7,9 @@ subjects=(sub-RC4111 sub-RC4119 sub-RC4210 sub-RC4217)
 for subject in ${subjects[@]} ; 
 do
 	echo "Processing >>> ${subject} <<<"
+    echo ""
     cmd="dwidenoise ${subject}/ses-1/dwi/${subject}*dwi*.nii.gz derivatives/${subject}/ses-1/dwi/denoised_dwi.nii.gz -noise derivatives/${subject}/ses-1/dwi/${subject}_noise.nii.gz -force"
+    echo ""
     echo $cmd
     eval $cmd
 done
