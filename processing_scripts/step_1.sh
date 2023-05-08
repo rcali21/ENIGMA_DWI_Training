98d53bc54276f9330edc35d725fc54fdff4d5eae
Ryan Cali <ryancali@PaulThoRyansMBP.fios-router.home>
2023-05-07 18:18:24 -0700

    added residuals to step_1

--- processing_scripts/step_1.sh
+++ processing_scripts/step_1.sh
@@ -8,9 +8,17 @@ for subject in ${subjects[@]} ;
 do
 	echo "Denoising >>> ${subject} <<<"
     echo ""
+    # Denoise image
     cmd="dwidenoise ${subject}/ses-1/dwi/${subject}*dwi*.nii.gz derivatives/${subject}/ses-1/dwi/denoised_dwi.nii.gz -noise derivatives/${subject}/ses-1/dwi/${subject}_noise.nii.gz -force"
     echo ""
     echo $cmd
     eval $cmd
+
+    # Generate residuals (This allows us to view what denoising did to our data)
+    cmd1="mrcalc ${subject}/ses-1/dwi/${subject}_ses-1_dwi.nii.gz derivatives/${subject}/ses-1/dwi/denoised_dwi.nii.gz -subtract derivatives/${subject}/ses-1/dwi/denoised_residuals.nii.gz"
+    echo ""
+    echo $cmd1
+    eval $cmd1
+
 done
 
