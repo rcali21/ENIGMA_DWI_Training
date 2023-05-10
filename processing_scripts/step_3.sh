de2d0102bfd71c3e96eda02254cb08547562e1dc
Ryan Cali <71532882+rcali21@users.noreply.github.com>
2023-05-09 19:47:06 -0700

    Update step_3.sh

--- processing_scripts/step_3.sh
+++ processing_scripts/step_3.sh
@@ -6,7 +6,7 @@ subjects=(sub-RC4111 sub-RC4119 sub-RC4210 sub-RC4217)
 
 for subject in ${subjects[@]} ; 
 do
-	echo "Removing Gibbs ringing artifacts from >>> ${subject} <<<"
+	echo "Performing brain extraction on >>> ${subject} <<<"
     echo ""
     cmd="bet derivatives/${subject}/ses-1/dwi/dendg_dwi.nii.gz derivatives/${subject}/ses-1/dwi/dendg_dwi_brain.nii.gz -m -f 0.2"
     echo ""
