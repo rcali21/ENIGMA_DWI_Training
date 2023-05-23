73556585d4ee8681aac97a6c59c53058b497884a
Ryan Cali <ryancali@PaulThoRyansMBP.fios-router.home>
2023-05-23 11:12:14 -0700

    Updated dwi2mask commands

--- processing_scripts/step_3.sh
+++ processing_scripts/step_3.sh
@@ -9,7 +9,7 @@ for subject in ${subjects[@]} ;
 do
 	echo "Performing brain extraction on >>> ${subject} <<<"
     echo ""
-    cmd="dwi2mask derivatives/${subject}/ses-1/dwi/dendg_dwi.nii.gz derivatives/${subject}/ses-1/dwi/dendg_dwi_mask.nii.gz -fslgrad ${subject}/ses-1/dwi/bvecs ${subject}/ses-1/dwi/bvals"
+    cmd="dwi2mask derivatives/${subject}/ses-1/dwi/dendg_dwi.nii.gz derivatives/${subject}/ses-1/dwi/dendg_dwi_mask.nii.gz -fslgrad ${subject}/ses-1/dwi/*bvec ${subject}/ses-1/dwi/*bval"
     echo $cmd
     eval $cmd
 
